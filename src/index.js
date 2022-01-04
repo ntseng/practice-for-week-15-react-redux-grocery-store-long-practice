407cd5f66a9a9c64ffa6f4c20d997ddc64f3e3d5
nathaniel <nathanieltseng@gmail.com>
2022-01-04 10:53:54 -0800

    phase 1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import configureStore from "./store";
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+	window.store = store;
+}
+
 function Root() {
-  return (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  );
+	return (
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	);
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Root />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
